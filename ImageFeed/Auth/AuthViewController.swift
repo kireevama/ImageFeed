0f614445a362cd58eedebf47bb1f5b0ab1a86bbf
kireevama <kireevama43@yandex.ru>
2025-01-14 08:35:02 +0300

    Исправлено название showWebViewSegueIdentifier на корректное

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -12,7 +12,7 @@ protocol AuthViewControllerDelegate: AnyObject {
 }
 
 final class AuthViewController: UIViewController {
-    private let showWebViewSegueIdentifier = "ShowWebView"
+    private let showWebViewSegueIdentifier = "showWebView"
     private let oauth2Service = OAuth2Service.shared
     
     weak var delegate: AuthViewControllerDelegate?
